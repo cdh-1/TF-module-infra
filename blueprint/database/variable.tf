8a3fed78e5f4e80b0367845119f8dc3023590808
cdh-1 <loganbowers451@gmail.com>
2021-11-13 07:45:44 -0500

    pull request

---
+++ blueprint/database/variable.tf
@@ -0,0 +1,12 @@
+
+
+// variable project {
+//   default = "terraform-architecture"
+
+// }
+
+// variable "delete_contents_on_destroy {
+//     description = "Optional"
+//     type = bool
+//     default = null
+// }
